a1b7a1e98d1eda3f2a1f905933097ed18cd96242
OleksiiLytvynenko <alexallmighty003@gmail.com>
2025-01-20 22:32:24 +0200

    Refactor code

--- src/UI/GUI.java
+++ src/UI/GUI.java
@@ -4,6 +4,8 @@ import Cipher.CaesarCipher;
 import FileService.FileService;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -50,50 +52,79 @@ public class GUI extends JFrame implements ActionListener{
 
         setVisible(true);
 
+        filePathField.getDocument().addDocumentListener(new ButtonStateController());
+        keyField.getDocument().addDocumentListener(new ButtonStateController());
+
+        updateButtonState();
+
+    }
+
+    private void updateButtonState() {
+        boolean isFilePathEmpty = filePathField.getText().trim().isEmpty();
+        boolean isKeyEmpty = keyField.getText().trim().isEmpty();
+        encryptButton.setEnabled(!isFilePathEmpty && !isKeyEmpty);
+        decryptButton.setEnabled(!isFilePathEmpty && !isKeyEmpty);
+        bruteforceButton.setEnabled(!isFilePathEmpty);
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String filePath = filePathField.getText();
-        CaesarCipher caesar = new CaesarCipher();
-        FileService fileService = new FileService();
+        try {
+            String filePath = filePathField.getText();
+            CaesarCipher caesar = new CaesarCipher();
+            FileService fileService = new FileService();
 
-        if (e.getSource() == encryptButton) {
-            int key = Integer.parseInt(keyField.getText());
-            String encryptedText = caesar.encrypt(fileService.readFile(filePath), key);
-            fileService.writeEncryptedFile(filePath, caesar.encrypt(fileService.readFile(filePath), key));
-            JOptionPane.showMessageDialog(this, "File encrypted successfully!");
-            JTextArea textArea = new JTextArea(encryptedText);
-            JScrollPane scrollPane = new JScrollPane(textArea);
-            textArea.setLineWrap(true);
-            textArea.setWrapStyleWord(true);
-            scrollPane.setPreferredSize(new Dimension(500, 500));
-            JOptionPane.showMessageDialog(this, scrollPane, "Encrypted text", JOptionPane.INFORMATION_MESSAGE);
+            if (e.getSource() == encryptButton) {
+                int key = Integer.parseInt(keyField.getText());
+                String encryptedText = caesar.encrypt(fileService.readFile(filePath), key);
+                fileService.writeEncryptedFile(filePath, caesar.encrypt(fileService.readFile(filePath), key));
+                showMessageDialog("File encrypted successfully!", encryptedText, "Encrypted text");
 
-        } else if (e.getSource() == decryptButton) {
-            int key = Integer.parseInt(keyField.getText());
-            String decryptedText = caesar.decrypt(fileService.readFile(filePath), key);
-            fileService.writeDecryptedFile(filePath, caesar.decrypt(fileService.readFile(filePath), key));
-            JOptionPane.showMessageDialog(this, "File decrypted successfully!");
-            JTextArea textArea = new JTextArea(decryptedText);
-            JScrollPane scrollPane = new JScrollPane(textArea);
-            textArea.setLineWrap(true);
-            textArea.setWrapStyleWord(true);
-            scrollPane.setPreferredSize(new Dimension(500, 500));
-            JOptionPane.showMessageDialog(this, scrollPane, "Decrypted text", JOptionPane.INFORMATION_MESSAGE);
+            } else if (e.getSource() == decryptButton) {
+                int key = Integer.parseInt(keyField.getText());
+                String decryptedText = caesar.decrypt(fileService.readFile(filePath), key);
+                fileService.writeDecryptedFile(filePath, caesar.decrypt(fileService.readFile(filePath), key));
+                showMessageDialog("File decrypted successfully!", decryptedText, "Decrypted text");
 
-        } else if (e.getSource() == bruteforceButton) {
-            int key = caesar.getKeyWithBruteforce(fileService.readFile(filePath));
-            String decryptedText = caesar.decrypt(fileService.readFile(filePath), key);
-            fileService.writeBruteForcedFile(filePath, fileService.readFile(filePath), key);
-            JOptionPane.showMessageDialog(this, "BruteForce was Successful!");
-            JTextArea textArea = new JTextArea(decryptedText);
-            JScrollPane scrollPane = new JScrollPane(textArea);
-            textArea.setLineWrap(true);
-            textArea.setWrapStyleWord(true);
-            scrollPane.setPreferredSize(new Dimension(500, 500));
-            JOptionPane.showMessageDialog(this, scrollPane, "BruteForced text, Key is - " + key, JOptionPane.INFORMATION_MESSAGE);
+            } else if (e.getSource() == bruteforceButton) {
+                int key = caesar.getKeyWithBruteforce(fileService.readFile(filePath));
+                String decryptedText = caesar.decrypt(fileService.readFile(filePath), key);
+                fileService.writeBruteForcedFile(filePath, fileService.readFile(filePath), key);
+                showMessageDialog("BruteForce was Successful!", decryptedText, "BruteForced text, Key is - " + key);
+            }
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(this, "Invalid key. Please enter a valid integer.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (HeadlessException ex) {
+            JOptionPane.showMessageDialog(this, "An error occurred: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void showMessageDialog(String message, String encryptedText, String Encrypted_text) {
+        JOptionPane.showMessageDialog(this, message);
+        JTextArea textArea = new JTextArea(encryptedText);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        scrollPane.setPreferredSize(new Dimension(500, 500));
+        JOptionPane.showMessageDialog(this, scrollPane, Encrypted_text, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private class ButtonStateController implements DocumentListener {
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            updateButtonState();
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            updateButtonState();
+        }
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            updateButtonState();
         }
     }
 }
